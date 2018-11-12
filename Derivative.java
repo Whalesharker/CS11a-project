2118c066b378ecfb8ce557a59af336753793cf89
Ian <Ian@micrognomics.com>
2018-11-12 10:19:51 -0500

    Added Derivative class just in case we decide to go with that.
    Also imported TextIO.

---
+++ Derivative.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+//for now let's have this just in case we need it.
+public class Derivative{
+  public static void main(String[] args) {
+    System.out.println("Hello world");
+  }
+  public String powerRule(String function){
+
+  }
+}
