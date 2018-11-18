b0a2c12353bc01e0b9ad8b898d3ce19f2d22080d
Ian <Ian@micrognomics.com>
2018-11-17 21:23:03 -0500

    Added a very messy pawn class. Also removed static from moveValid
    I don't know if this works at all.

---
+++ Pawn.java
@@ -0,0 +1,48 @@
+public class Pawn extends Piece{
+  //This is inherited so it should have all the functions of the base piece unless overwritten.
+  public String type = "P";
+  public boolean firstmove = true;
+  public static void main(String[] args) {
+    System.out.println("Pawns can move 1 space forward or 2 spaces on their first move. However they can only capture pieces by moving 1 space diagnoally");
+  }
+  public boolean moveValid(int x, int y, int newx, int newy, Piece[][] board){
+    boolean good = true;
+    if (Math.abs(x - newx) > 1){
+      good = false;
+      //Pawns will never be able to move more than 1 space across.
+    }
+    else if((Math.abs(y - newy) > 2) || (Math.abs(y-newy) <=0)){
+      //moving the pawn more than 2 spaces or less than 1 space verticly.
+      //TODO: make it so that black pawns can only move down and white pawns can only move up.
+      good = false;
+    }
+    else if(Math.abs(y-newy) == 2 && firstmove == false){
+      good = false;
+    }
+    else if(Math.abs(x - newx) == 1 && Math.abs(y-newy) >= 2){
+      good = false;
+      //Moving the pawn 2 spaces up and also diagnoally isn't allowed..
+    }
+
+    //if it clears all the instant nopes it should run this:
+    else if (x - newx == 0){
+      //this is the scenario for the pawn just moving straight.
+      for(int i = y; i<=newy;i++){
+        if(board[x][y].getType() !="e"){
+          good = false;
+          //returns false if any of the tiles from the current location to the new location are occupied.
+        }
+      }
+    }
+    else if(Math.abs(x - newx) == 1 && board[newx][newy].getType()== "e"){
+      //scenario for the pawn capturing something by moving diagnoally.
+      //checks to make sure that it's moving one across and that there's a piece where it's trying to capture.
+      //This probably could just be an else statement, but I wanted to do one final check just in case I missed something.
+    }
+    if(good == true){
+      firstmove = false;
+      //notes that the pawn has made the first move if it successfully moves.
+    }
+    return good;
+  }
+}
--- Piece.java
+++ Piece.java
@@ -5,7 +5,7 @@ public class Piece{
   public static void main(String[] args) {
     System.out.println("You aren't supposed to run the piece by itself.");
   }
-  public static boolean moveValid(int x, int y, int newx, int newy, Piece[][] board){
+  public boolean moveValid(int x, int y, int newx, int newy, Piece[][] board){
     //Takes the current x and y coordinates of the piece and the new x and y it wants to move too along with the board itself.
     //This should examine each tile inbetween the piece and the destination to see if there's anything blocking it.
     //it should also check if that's actually a valid move for that piece.
