e6982e86ca3d0d27ce25cc1496e0c810af94a135
Ian <Ian@micrognomics.com>
2018-11-17 20:34:12 -0500

    Added class for base chess piece.
    This actually represents and empty tile and will be the parent class for the other pieces.

---
+++ Piece.java
@@ -0,0 +1,25 @@
+public class Piece{
+  public String type = "e";
+  public String color = "";
+  //Color is initially nothing. It should be set later.
+  public static void main(String[] args) {
+    System.out.println("You aren't supposed to run the piece by itself.");
+  }
+  public static boolean moveValid(int x, int y, int newx, int newy, Piece[][] board){
+    //Takes the current x and y coordinates of the piece and the new x and y it wants to move too along with the board itself.
+    //This should examine each tile inbetween the piece and the destination to see if there's anything blocking it.
+    //it should also check if that's actually a valid move for that piece.
+    //In the BASE piece class all moves should be invalid, each type of piece should have it's own function for movevalid
+    return false;
+    //honestly guys I don't know when to and not to use static variables.
+    //I've just been erasing the static when I get an error.
+  }
+  public String getType(){
+    return type;
+    //returns a letter representing the piece. For the base piece it should be "e" to represent "empty"
+  }
+  public void setColor(String c){
+    color = c;
+    //a color is passed to this during the intial setup.
+  }
+}
